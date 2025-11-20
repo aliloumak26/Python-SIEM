43802254de0e3a06e51d423b80fe56f6843d3274
omarrayane <155535682+omarrayane@users.noreply.github.com>
2025-11-20 15:46:37 +0100

    Update crlf.py

--- detectors/crlf.py
+++ detectors/crlf.py
@@ -22,13 +22,28 @@ PATTERNS = [
     r"\r\n",
     r"\r",
     r"\n",
+    r"set-cookie:.*\r\n",
+    r"location:.*\r\n",
+    r"content-length:.*\r\n",
+    r"\u000d",
+    r"\u000a",
+    r"%0d%0d%0a",
+    r"%0a%0a%0d",
+    r"[\r\n]+.*:",
+
+
+
+
 ]
 
 def detect(line):
     text = normalize(line)
+    matches = []
     
     for p in PATTERNS:
         if re.search(p, text, re.IGNORECASE):
-            return True, p, "CRLF Injection"
+            matches.append(p)
+    if matches:
+        return True, matches, "CRLF Injection"
 
     return False, None, None
