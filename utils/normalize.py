9a443a45f7457638d8d701b65e80707a3be373cb
omarrayane <155535682+omarrayane@users.noreply.github.com>
2025-11-20 15:00:52 +0100

    Update normalize.py

--- utils/normalize.py
+++ utils/normalize.py
@@ -1,3 +1,4 @@
+import html
 from urllib.parse import unquote
 
 def normalize(text: str) -> str:
@@ -5,8 +6,12 @@ def normalize(text: str) -> str:
         return ""
     try:
         decoded = unquote(text)
+        decoded =unquote(decoded)  # Decode twice to handle double-encoding
+        decoded = html.unescape(decoded) #this one  for html entities
+        decoded = decoded.encode('utf-8').decode('unicode_escape')
         decoded = decoded.replace('\\"', '"').replace("\\'", "'")
         decoded = decoded.replace("\\", "")
+        decoded = " ".join(decoded.split())
         return decoded.lower().strip()
     except:
         return text.lower().strip()
