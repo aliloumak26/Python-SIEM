ffd9f77fc9ccd2da3186fdb30e967584e0bfa7a0
L7out4 <155485437+aliloumak26@users.noreply.github.com>
2025-11-20 13:34:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from core.log_reader import watch_access_log
 
 if __name__ == "__main__":
-    print("🚀 SIEM started. Watching access.log...")
+    print("SIEM started. Watching access.log...")
     watch_access_log()
