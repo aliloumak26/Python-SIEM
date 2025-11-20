00a3af7d333a9ece957859fe69ef4ce0062163af
omarrayane <155535682+omarrayane@users.noreply.github.com>
2025-11-20 15:54:25 +0100

    Update os_injection.py

--- detectors/os_injection.py
+++ detectors/os_injection.py
@@ -1,37 +1,48 @@
+import os
+import time
 import re
 from utils.normalize import normalize
+from config.settings import settings
 
+LOG_PATH = settings.ACCESS_LOG_PATH
+
+# ----------------- OS COMMAND INJECTION PATTERNS -----------------
+PATTERNS = [
+    r";",
+    r"&&",
+    r"&",
+    r"\|\|",
+    r"\|",
+    r"\$\(",
+    r"`",
+    r"%3b",
+    r"%26",
+    r"%7c",
+    r"%24%28",
+    r"\$path",
+    r"\$home",
+    r"\$user",
+    r"\$shell",
+    r"\brm\b",
+    r"\bls\b",
+    r"bwhoami\b",
+    r"\bcat\b",
+    r"\bhmod\b",
+    r"\bcurl\b",
+    r"\bwget\b",
+    r"\bping\b",
+    r"\bshutdown\b",
+]
+
+    
 def detect(line):
-    patterns = [
-        r";",
-        r"&&",
-        r"&",
-        r"\|\|",
-        r"\|",
-        r"\$\(",
-        r"`",
-        r"%3b",
-        r"%26",
-        r"%7c",
-        r"%24%28",
-        r"\$path",
-        r"\$home",
-        r"\$user",
-        r"\$shell",
-        r"\brm\b",
-        r"\bls\b",
-        r"bwhoami\b",
-        r"\bcat\b",
-        r"\bhmod\b",
-        r"\bcurl\b",
-        r"\bwget\b",
-        r"\bping\b",
-        r"\bshutdown\b",
-    ]
+    text = normalize(line)
+    matches = []
     
-    for p in patterns:
-        text = normalize(line)
+    for p in PATTERNS:
         if re.search(p, text, re.IGNORECASE):
-            return True, p, "OS_command_Injection_detected"
-    
+            matches.append(p)
+    if matches:
+        return True, matches, "OS Command Injection"
+
     return False, None, None
